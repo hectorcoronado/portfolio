41f1bf26eee3429875df75326fb762b013d196d3
hcoronado <hcoronado@nolo.com>
2019-01-14 16:00:36 -0800

    refines styles for rectangles, WIP navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navigation = () => (
-  <nav>
-    <NavLink to='/' activeClassName='is-active' exact>home</NavLink>
-    <NavLink to='/about' activeClassName='is-active' exact>about</NavLink>
-    <NavLink to='/projects' activeClassName='is-active' exact>projects</NavLink>
-    <NavLink to='/contact' activeClassName='is-active' exact>contact</NavLink>
+  <nav className='navigation'>
+    <NavLink to='/' className='navigation__link' activeClassName='is-active' exact>home</NavLink>
+    <NavLink to='/about' className='navigation__link' activeClassName='is-active' exact>about</NavLink>
+    <NavLink to='/projects' className='navigation__link' activeClassName='is-active' exact>projects</NavLink>
+    <NavLink to='/contact' className='navigation__link' activeClassName='is-active' exact>contact</NavLink>
   </nav>
 )
 
