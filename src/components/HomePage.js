e019a7443054735c53a868cf3dc47cb75eb77cb2
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-01-16 18:55:27 -0800

    scaffolds PrimaryText component & styles

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import PrimaryRectangle from './PrimaryRectangle'
+import PrimaryText from './PrimaryText'
 import SecondaryRectangle from './SecondaryRectangle'
 
 const HomePage = () => (
   <div className='rectangle-container'>
-    <PrimaryRectangle />
+    <PrimaryRectangle>
+      <PrimaryText text='hector coronado' />
+      <PrimaryText text='web developer' />
+    </PrimaryRectangle>
     <SecondaryRectangle />
   </div>
 )
---
+++ src/components/PrimaryText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrimaryText = props => (
+    <h2 className='primary-text'>
+        { props.text }
+    </h2>
+)
+
+export default PrimaryText
