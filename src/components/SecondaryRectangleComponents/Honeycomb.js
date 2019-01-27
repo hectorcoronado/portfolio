203d60ceaa7b39c6cd1a135e7e12181704b419c9
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-01-26 16:07:15 -0800

    renders responsive honeycomb

--- src/components/SecondaryRectangleComponents/Honeycomb.js
+++ src/components/SecondaryRectangleComponents/Honeycomb.js
@@ -23,9 +23,40 @@ class Honeycomb extends Component {
         })
     }
     
-    drawRectInCanvas () {
+    drawHoneycomb () {
         const hCtx = this.honeycombRef.current.getContext('2d')
-        hCtx.fillRect(0, 0, this.state.width, this.state.height)
+        const h = hCtx.canvas.height
+        const w = hCtx.canvas.width
+        const yd = 16
+        const xd = 2 * yd * Math.sin(Math.PI / 3)
+        const numHexW = Math.ceil(w  / (2 * xd)) + 1
+        const numHexH = Math.ceil(h  / (3 * yd)) + 1
+        
+        hCtx.strokeStyle = 'rgba(2, 6, 35, 1)'
+        hCtx.lineWidth = 1
+        
+        hCtx.beginPath()
+        
+        for (let i = 0; i < numHexH; i += 1) {
+            const yBase = (i * 3 + 2) * yd - i % 2 * yd
+            hCtx.moveTo(0, yBase)
+            for (let j = 1; j <= numHexW; j += 1) {
+                if (i % 2) {
+                    hCtx.lineTo((2 * j - 1) * xd, yBase + yd)
+                    hCtx.lineTo(2 * j * xd, yBase)
+                    hCtx.moveTo(2 * j * xd, yBase - 2 * yd)
+                    hCtx.lineTo(2 * j * xd, yBase)
+                } else {
+                    hCtx.lineTo((2 * j - 1) * xd, yBase - yd)
+                    hCtx.lineTo((2 * j - 1) * xd, yBase - 3 * yd)
+                    hCtx.moveTo((2 * j - 1) * xd, yBase - yd)
+                    hCtx.lineTo(2 * j * xd, yBase)
+                }
+            }
+        }
+
+        hCtx.stroke()
+        hCtx.closePath()
     }
     
     componentDidMount() {
@@ -34,7 +65,7 @@ class Honeycomb extends Component {
     }
     
     componentDidUpdate () {
-        this.drawRectInCanvas()
+        this.drawHoneycomb()
     }
 
     componentWillUnmount () {
