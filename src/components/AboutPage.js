bab3b15413d37164fb772b6e5bc964cc6077b3d2
hectorcoronado <hectorcoronado.developer@gmail.com>
2019-02-03 11:10:01 -0800

    styles home & about components

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -9,13 +9,13 @@ import SecondaryText from './SecondaryText';
 class AboutPage extends Component {
     static defaultProps = {
         name: 'hector coronado',
-        aboutText: 'i am a front-end developer at martindale-nolo, where i work with a prodigious team to build fast ui\'s that help people find medical & legal services. i work most often in javascript/react, & love css. when i\'m not doing that, i run to stay in shape & watch at least one movie per week.'
+        aboutText: 'i am a front-end developer at martindale-nolo, where i work with a prodigious team to build fast ui\'s that help people find medical & legal services. i work mostly in javascript/react, & love css. avid runner & amateur film critic.'
     }
 
     render () {
         return (
             <div className='rectangle-container'>
-                <PrimaryRectangle>
+                <PrimaryRectangle modifier='primary-rectangle--about'>
                     <PrimaryText text={this.props.name} />
                     <SecondaryText text={this.props.aboutText} modifier='secondary-text--about'/>
                 </PrimaryRectangle>
--- src/components/PrimaryRectangle.js
+++ src/components/PrimaryRectangle.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PrimaryRectangle = props => (
-    <div className='primary-rectangle'>
+    <div className={`primary-rectangle${props.modifier ? ` ${props.modifier}` : ''}`}>
         { props.children }
     </div>
 )
